a7c81e6fb374e95a6b1f7c4f1be145c365cdb2dc
abdras <aabdras@yandex.kz>
2025-05-25 00:04:05 +0500

    Исправление githubActions

--- test/manager/test/InMemoryHistoryManagerTest.java
+++ test/manager/test/InMemoryHistoryManagerTest.java
@@ -8,23 +8,6 @@ import java.util.ArrayList;
 
 class InMemoryHistoryManagerTest {
 
-
-    @Test
-    public void shouldNotUpdateTasksWhenAddHistory() {
-        HistoryManager historyManager = Managers.getDefaultHistory();
-        TaskManager manager = Managers.getDefault();
-
-        Task task1 = new Task(1, "task1", "desc1", Status.NEW);
-        Task task3 = new Task(2, "task3", "desc3", Status.NEW);
-
-        historyManager.addTask(task1);
-        manager.updateTask(task3);
-        System.out.println(historyManager.getHistory().getFirst());
-        System.out.println(historyManager.getHistory().get(1));
-        Assertions.assertEquals(historyManager.getHistory().getFirst(), historyManager.getHistory().get(1));
-    }
-
-
     @Test
     public void shouldNotRemoveHistoryWhenAddTasks() {
         HistoryManager historyManager = Managers.getDefaultHistory();
